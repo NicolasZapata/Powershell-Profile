3655ffac1ceb9c1610c56715e8777bb1571831e0
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2022-02-18 11:43:57 -0500

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+#Alias
+Set-Alias vim nvim
+Set-Alias pl pdflatex
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+#Reduce path function
+function prompt {
+  $p = Split-Path -leaf -path (Get-Location)
+  "$p
+  -> "
+}
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
